290e815efb281e0f9343e89bd2ce688f50072016
Richard <richardmusime6@gmail.com>
2024-05-27 20:26:25 +0200

    Adding The finding the smallest interger in array in array

---
+++ 8kyu/smallestinterger.js
@@ -0,0 +1,11 @@
+function findSmallestInt(arr) {
+    let result = arr[0]
+    for(let items of arr){
+      if(items < result){
+        result = items
+      }
+    }
+    return result
+  }
+  console.log(findSmallestInt([34, 15, 88, 2]));
+  console.log(findSmallestInt([34, -345, -1, 100]))
