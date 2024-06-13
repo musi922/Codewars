c4d3f1beb503a757ddaa50a6cf1c65d0d60c05cc
Richard <richardmusime6@gmail.com>
2024-06-13 08:43:57 +0200

    added a simplest way using Es6 features such as filter,sort and map to make codes look much easier and minimize them

--- 6kyu/Differenceof2.js
+++ 6kyu/Differenceof2.js
@@ -1,17 +1,24 @@
-function twosDifference(input){
-    let result = [];
-    for (let i=0;i<input.length;i++){
-        for(let j=0;j<input.length;j++){
-            if(input[j]-input[i]===2){
-                result.push([input[i],input[j]])
-            }
-        }
-    }
-    return result.sort((a,b)=>a[0]-b[0])
+// function twosDifference(input){
+//     let result = [];
+//     for (let i=0;i<input.length;i++){
+//         for(let j=0;j<input.length;j++){
+//             if(input[j]-input[i]===2){
+//                 result.push([input[i],input[j]])
+//             }
+//         }
+//     }
+//     return result.sort((a,b)=>a[0]-b[0])
 
-  }
-  console.log(twosDifference([1, 2, 3, 4]));
-  console.log(twosDifference([4, 1, 2, 3]));
-  console.log(twosDifference([1, 23, 3, 4, 7]));
-  console.log(twosDifference([4, 3, 1, 5, 6]));
-  console.log(twosDifference([1,3,5,6,8,10,15,32,12,14,56]));
+//   }
+//   console.log(twosDifference([1, 2, 3, 4]));
+//   console.log(twosDifference([4, 1, 2, 3]));
+//   console.log(twosDifference([1, 23, 3, 4, 7]));
+//   console.log(twosDifference([4, 3, 1, 5, 6]));
+//   console.log(twosDifference([1,3,5,6,8,10,15,32,12,14,56]));
+
+
+  //simple way
+let twosDifference=(input)=>input.filter((a)=>input.includes(a+2)).sort((x,y)=>x-y).map(a=>[a,a+2])
+console.log(twosDifference([1, 2, 3, 4]));
+
+
