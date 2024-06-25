17f01e752b291596a097db9c09f76c368a46432f
Richard <richardmusime6@gmail.com>
2024-06-25 09:51:52 +0200

    Added a sort santa's reinder 7kyu kata

---
+++ 6kyu/BasicBoundingBox.js
@@ -0,0 +1,18 @@
+function sortReindeer(reindeerNames) {
+    let result = []
+    for (let i = 0; i< reindeerNames.length; i++) {
+        result.push(reindeerNames[i].split(' '))
+        }
+        
+   return result.sort((a,b)=> a[1] < b[1] ? -1 :1).map(i => i[0]+" "+i[1])
+   }
+   console.log(sortReindeer([
+       "Dasher Tonoyan", 
+       "Dancer Moore", 
+       "Prancer Chua", 
+       "Vixen Hall", 
+       "Comet Karavani",        
+       "Cupid Foroutan", 
+       "Donder Jonker", 
+       "Blitzen Claus"
+     ]));
