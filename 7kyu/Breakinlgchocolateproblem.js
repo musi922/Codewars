208637ec7e7a69036c6eecbbd03334f989ad4c78
Richard <richardmusime6@gmail.com>
2024-07-17 11:06:33 +0200

    Added a kata for breaking the chocolate into bars if the bars are breakable

---
+++ 7kyu/Breakinlgchocolateproblem.js
@@ -0,0 +1,8 @@
+function breakChocolate(n, m) {
+    if (n <= 1 && n <= 1) {
+        return 0;
+    }
+    var res = n * m;
+    return res - 1;
+}
+console.log(breakChocolate(5, 5));
---
+++ 7kyu/Breakinlgchocolateproblem.ts
@@ -0,0 +1,9 @@
+function breakChocolate(n: number, m: number): number {
+    if (n <= 1 && n<=1) {
+        return 0  
+    }
+    let res:number = n*m
+    return res - 1
+  }
+  console.log(breakChocolate(5,5));
+  
