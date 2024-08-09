9be080e7949f17ffe9919c2e6cffa6915c0169c5
Richard <richardmusime6@gmail.com>
2024-08-09 16:55:26 +0200

    Added the kata that  Help Alex to know the score of matches.

--- 6kyu/riemannsums.js
+++ 6kyu/riemannsums.js
@@ -1,5 +1,16 @@
-function riemann_trapezoidal(f, n, a, b){
-	let sum = 0;
- 
-    return sum;
+function frame(score) {
+    const frames = score.split(';').filter(f => f.trim() !== '');
+    let player1Wins = 0;
+    let player2Wins = 0;
+    
+    frames.forEach(frame => {
+        const [player1, player2] = frame.split('-').map(part => parseInt(part));
+        if (player1 > player2) {
+            player1Wins++;
+        } else {
+            player2Wins++;
+        }
+    });
+    
+    return [player1Wins, player2Wins];
 }
