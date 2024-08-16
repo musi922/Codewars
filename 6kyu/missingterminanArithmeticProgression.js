feb3d81356162b4d7590a4060637c66c50ebc6f6
Richard <richardmusime6@gmail.com>
2024-08-16 15:20:24 +0200

    Added a kata for  a function that receives a list, list size will always be at least 3 numbers. and return missing value

---
+++ 6kyu/missingterminanArithmeticProgression.js
@@ -0,0 +1,18 @@
+const findMissing = function (list) {  
+    const diff1 = list[1] - list[0];
+    const diff2 = list[2] - list[1];
+    
+    const commonDiff = diff1 === diff2 ? diff1 : list[2] - list[0] === 2 * diff1 ? diff1 : diff2;
+    
+    for (let i = 0; i < list.length - 1; i++) {
+        if (list[i + 1] - list[i] !== commonDiff) {
+            return list[i] + commonDiff;
+        }
+    }
+    
+    return list[list.length - 1] + commonDiff;
+}
+
+console.log(findMissing([1, 3, 5, 9, 11])); 
+console.log(findMissing([1, 5, 7, 9, 11])); 
+console.log(findMissing([1, 3, 4]));
