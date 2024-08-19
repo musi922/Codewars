6e0e8b066b00a26d4e90e382f10a0fc023565b66
Richard <richardmusime6@gmail.com>
2024-08-19 10:17:06 +0200

    Added the kata for vector affinity

--- 6kyu/Vector Affinity.js
+++ 6kyu/Vector Affinity.js
@@ -1,3 +1,22 @@
-function vectorAffinity(xs,ys) {
-    return 0;
-  }
+function vectorAffinity(xs, ys) {
+    if (xs.length === 0 && ys.length === 0) {
+        return 1;
+    }
+
+    const minLength = Math.min(xs.length, ys.length);
+    const maxLength = Math.max(xs.length, ys.length);
+    
+    let matchCount = 0;
+    for (let i = 0; i < minLength; i++) {
+        if (xs[i] === ys[i]) {
+            matchCount++;
+        }
+    }
+    
+    return matchCount / maxLength;
+}
+
+console.log(vectorAffinity([1, 2, 3, 4, 5], [1, 2, 2, 4, 3]));
+console.log(vectorAffinity([1, 2, 3], [1, 2, 3]));
+console.log(vectorAffinity([1, 2, 3], [1, 2, 3, 4, 5]));
+console.log(vectorAffinity([], []));
