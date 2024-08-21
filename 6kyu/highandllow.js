b5b1de8372b75dcf65cccef0e1439401af9a29ce
Richard <richardmusime6@gmail.com>
2024-08-21 14:20:52 +0200

    Added a kata for finding the highest and smallest number in string

---
+++ 6kyu/highandllow.js
@@ -0,0 +1,10 @@
+function highAndLow(numbers){
+    const numArray = numbers.split(" ").map(Number)
+    const high = Math.max(...numArray);
+    const low = Math.min(...numArray);
+    return `${high} ${low} `
+  
+  }
+
+  console.log(highAndLow("1 2 3 4 5"));
+  
